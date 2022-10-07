186e9c6893819192317c8ef76bb3a0562a7c2089
Ruan Carlos <ruan.espindola17@gmail.com>
2022-10-07 17:34:43 -0300

    Formulario HTML

---
+++ script.js
@@ -0,0 +1,37 @@
+const init = () => {
+    const inputEmail = document.querySelector('input[type="email"]');
+    const inputPassword = document.querySelector('input[type="password"}');
+    const SubmitButton = document.querySelector('login__submit');
+    
+ 
+    
+    if(SubmitButton) {
+        SubmitButton.addEventListener('click', (event) => {
+           event.preventDefault();
+
+           fetch('https:// reqres.in/api/login', {
+               method: 'POST',
+               headers: {
+                    'Content-Type': 'application/json'
+               },
+               body: JSON.stringify({
+                   email: inputEmail.value,
+                   password: inputPassword.value,
+
+               })
+           }).then((response) => {
+              return response.json();
+
+           }).then((data) => {
+              console.log(data)
+           })
+                    
+             
+                       
+        })
+
+    }
+
+}
+ 
+window.onload = init;
